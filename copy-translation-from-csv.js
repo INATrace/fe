0ff81cbbafa61be29e1d52dc973bbc04e7c628e8
Pece Adjievski <pece.adzievski@sunesis.si>
2021-08-19 21:56:16 +0200

    updated script for importing translations

--- copy-translation-from-csv.js
+++ copy-translation-from-csv.js
@@ -1,13 +1,16 @@
-const csvFile = './src/assets/locale/Coffee-translations.txt'; //copy from google sheets
+const csvFile = './src/assets/locale/Coffee-translations.csv'; //copy from google sheets
 const fs = require('fs')
 const readline = require('readline');
 
 const localeFolder = './src/assets/locale/';
 const enLocaleFile = './src/assets/locale/en.json';
 const deLocaleFile = './src/assets/locale/de.json';
+const esLocaleFile = './src/assets/locale/es.json';
 const rwLocaleFile = './src/assets/locale/rw.json';
-// var enJSON = JSON.parse(fs.readFileSync(enLocaleFile));
+
+var enJSON = JSON.parse(fs.readFileSync(enLocaleFile));
 var deJSON = JSON.parse(fs.readFileSync(deLocaleFile));
+var esJSON = JSON.parse(fs.readFileSync(esLocaleFile));
 var rwJSON = JSON.parse(fs.readFileSync(rwLocaleFile));
 
 const readTranslations = readline.createInterface({
@@ -16,29 +19,35 @@ const readTranslations = readline.createInterface({
   terminal: false
 });
 
-
 readTranslations.on('line', (line) => {
   splitLine = line.split(';');
 
   id = splitLine[0];
-  // en = splitLine[1];
+  en = splitLine[1];
   de = splitLine[2];
-  rw = splitLine[3];
+  es = splitLine[3];
+  rw = splitLine[4];
 
-  // let enData = enJSON.translations;
+  let enData = enJSON.translations;
   let rwData = rwJSON.translations;
   let deData = deJSON.translations;
+  let esData = esJSON.translations;
 
-  // enData[id] = en;
+  if (en) enData[id] = en;
   if (de) deData[id] = de;
+  if (es) esData[id] = es;
   if (rw) rwData[id] = rw;
 
-  // let outputJSONen = JSON.stringify(enJSON, null, 2);
-  // fs.writeFileSync(enLocaleFile, outputJSONen);
+  let outputJSONen = JSON.stringify(enJSON, null, 2);
+  fs.writeFileSync(enLocaleFile, outputJSONen);
+
   let outputJSONde = JSON.stringify(deJSON, null, 2);
   fs.writeFileSync(deLocaleFile, outputJSONde);
+
+  let outputJSONes = JSON.stringify(esJSON, null, 2);
+  fs.writeFileSync(esLocaleFile, outputJSONes);
+
   let outputJSONrw = JSON.stringify(rwJSON, null, 2);
   fs.writeFileSync(rwLocaleFile, outputJSONrw);
 
 });
-
