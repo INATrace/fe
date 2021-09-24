75d2f28c32cf5cc4f936bdc83f39d963c7e95a64
Pece Adjievski <pece.adzievski@sunesis.si>
2021-09-24 22:50:47 +0200

    WIP: Home page refactoring and components reorganization

--- src/app/m-product/m-product-routing.module.ts
+++ src/app/m-product/m-product-routing.module.ts
@@ -47,7 +47,6 @@ const routes: Routes = [
       drobtinice: null
     }
   },
-  { path: ':id/f-p-q-explorer', loadChildren: () => import('./f-p-q-explorer/f-p-q-explorer.module').then(m => m.FPQExplorerModule) },
   { path: ':id/knowledge-blog', loadChildren: () => import('./knowledge-blog/knowledge-block.module').then(m => m.KnowledgeBlockModule) },
   { path: ':id/orders', loadChildren: () => import('./product-orders/product-orders.module').then(m => m.ProductOrdersModule) },
   { path: ':id/stakeholders', loadChildren: () => import('./product-stakeholders/product-stakeholders.module')
