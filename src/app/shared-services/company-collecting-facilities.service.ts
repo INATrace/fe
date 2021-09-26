46786d7002d407b8872fa6c9554f9ce11ec07455
Pece Adjievski <pece.adzievski@sunesis.si>
2021-09-26 23:06:03 +0200

    added services for Facility

---
+++ src/app/shared-services/company-collecting-facilities.service.ts
@@ -0,0 +1,56 @@
+import { GeneralSifrantService } from './general-sifrant.service';
+import { ApiFacility } from '../../api/model/apiFacility';
+import { FacilityControllerService, ListCollectingFacilitiesByCompanyUsingGET } from '../../api/api/facilityController.service';
+import { Observable } from 'rxjs';
+import { PagedSearchResults } from '../../interfaces/CodebookHelperService';
+import { map } from 'rxjs/operators';
+import { ApiPaginatedResponseApiFacility } from '../../api/model/apiPaginatedResponseApiFacility';
+
+export class CompanyCollectingFacilitiesService extends GeneralSifrantService<ApiFacility> {
+
+  requestParams = {
+    limit: 1000,
+    offset: 0,
+  } as ListCollectingFacilitiesByCompanyUsingGET.PartialParamMap;
+
+  constructor(
+    private facilityControllerService: FacilityControllerService,
+    private companyId: number
+  ) {
+    super();
+  }
+
+  textRepresentation(el: ApiFacility): string {
+    return `${el.name}`;
+  }
+
+  identifier(el: ApiFacility) {
+    return el.id;
+  }
+
+  placeholder(): string {
+    return $localize`:@@activeCollectingFacility.input.placehoder:Select facility`;
+  }
+
+  makeQuery(key: string, params?: any): Observable<PagedSearchResults<ApiFacility>> {
+
+    const limit = params && params.limit ? params.limit : this.limit();
+    const reqParams = {
+      id: this.companyId,
+      ...this.requestParams
+    };
+
+    return this.facilityControllerService.listCollectingFacilitiesByCompanyUsingGETByMap(reqParams)
+      .pipe(
+        map((res: ApiPaginatedResponseApiFacility) => {
+          return {
+            results: res.data.items,
+            offset: 0,
+            limit,
+            totalCount: res.data.count
+          };
+        })
+      );
+  }
+
+}
