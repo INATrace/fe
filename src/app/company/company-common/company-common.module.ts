77d08aa3347d9461184e2bf66dba3629055c32b0
Pece Adjievski <pece.adzievski@sunesis.si>
2021-10-09 18:45:17 +0200

    added link to Company profile under company left panel

--- src/app/company/company-common/company-common.module.ts
+++ src/app/company/company-common/company-common.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { CompanyLeftPanelComponent } from './company-left-panel/company-left-panel.component';
 import { SharedModule } from '../../shared/shared.module';
 import { RouterModule } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
@@ -11,7 +12,8 @@ import { RouterModule } from '@angular/router';
   imports: [
     CommonModule,
     SharedModule,
-    RouterModule
+    RouterModule,
+    FontAwesomeModule
   ],
   exports: [
     CompanyLeftPanelComponent
