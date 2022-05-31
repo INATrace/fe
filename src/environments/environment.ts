d71931ec79d12e57adb1f2025aafab9061e728a3
Pece Adjievski <pece.adzievski@sunesis.si>
2022-05-31 11:12:24 +0200

    set hot-fix version

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -14,7 +14,7 @@ export const environment = {
     relativeFileUploadUrlManualType: window['env']['relativeFileUploadUrlManualType'] || '',
     relativeImageUploadUrl: window['env']['relativeImageUploadUrl'] || '',
     relativeImageUploadUrlAllSizes: window['env']['relativeImageUploadUrlAllSizes'] || '',
-    version: '2.11.1',
+    version: '2.11.2',
 
     googleMapsApiKey: window['env']['googleMapsApiKey'] || '',
     googleAnalyticsId: '',
