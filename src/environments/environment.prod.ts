c096395abf39795b9cfe2062cd07b574cc24337e
Pece Adjievski <pece.adzievski@sunesis.si>
2022-06-05 19:35:47 +0200

    set next dev version

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -14,7 +14,7 @@ export const environment = {
     relativeFileUploadUrlManualType: window['env']['relativeFileUploadUrlManualType'] || '',
     relativeImageUploadUrl: window['env']['relativeImageUploadUrl'] || '',
     relativeImageUploadUrlAllSizes: window['env']['relativeImageUploadUrlAllSizes'] || '',
-    version: '2.17.0-SNAPSHOT',
+    version: '2.18.0-SNAPSHOT',
 
     googleMapsApiKey: window['env']['googleMapsApiKey'] || '',
     googleAnalyticsId: '',
