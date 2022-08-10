e780d8084d97964fe74353bc95223174d58a9d88
Pece Adjievski <pece.adzievski@sunesis.si>
2022-08-10 22:15:23 +0200

    WIP: removed api-chain dependencies

--- src/app/company/company-customers/company-customers-details/company-customers-details.component.ts
+++ src/app/company/company-customers/company-customers-details/company-customers-details.component.ts
@@ -3,23 +3,16 @@ import _ from 'lodash-es';
 import { faTrashAlt } from '@fortawesome/free-regular-svg-icons';
 import { FormArray, FormGroup } from '@angular/forms';
 import { ApiCompany } from '../../../../api/model/apiCompany';
-import { ApiUserCustomer } from '../../../../api/model/apiUserCustomer';
 import { GlobalEventManagerService } from '../../../core/global-event-manager.service';
 import { ActivatedRoute } from '@angular/router';
-import { OrganizationService } from '../../../../api-chain/api/organization.service';
-import { CompanyCustomerService } from '../../../../api-chain/api/companyCustomer.service';
 import { AuthService } from '../../../core/auth.service';
 import { Location } from '@angular/common';
-import { ProductService } from '../../../../api-chain/api/product.service';
-import { SemiProductService } from '../../../../api-chain/api/semiProduct.service';
 import { take } from 'rxjs/operators';
-import { dbKey, defaultEmptyObject, generateFormFromMetadata } from '../../../../shared/utils';
+import { defaultEmptyObject, generateFormFromMetadata } from '../../../../shared/utils';
 import { CompanyControllerService } from '../../../../api/api/companyController.service';
 import { ApiCompanyCustomer } from '../../../../api/model/apiCompanyCustomer';
 import { ApiCompanyCustomerValidationScheme } from './validation';
 import { ApiGeoAddress } from '../../../../api/model/apiGeoAddress';
-import { ApiCountry } from '../../../../api/model/apiCountry';
-import { ApiAddress } from '../../../../api/model/apiAddress';
 
 @Component({
   selector: 'app-company-customers-details',
@@ -36,8 +29,6 @@ export class CompanyCustomersDetailsComponent implements OnInit {
   organization: ApiCompany;
   organizationId: string;
   productId;
-  chainProductId;
-  chainProduct;
 
   customer: ApiCompanyCustomer;
   customerId;
@@ -50,12 +41,8 @@ export class CompanyCustomersDetailsComponent implements OnInit {
   constructor(
       private globalEventsManager: GlobalEventManagerService,
       private route: ActivatedRoute,
-      private chainOrganizationService: OrganizationService,
-      private chainCompanyCustomerService: CompanyCustomerService,
       private authService: AuthService,
       private location: Location,
-      private chainProductService: ProductService,
-      private chainSemiProductService: SemiProductService,
       private companyService: CompanyControllerService
   ) { }
 
@@ -75,15 +62,9 @@ export class CompanyCustomersDetailsComponent implements OnInit {
     if (!action) { return; }
     this.organizationId = localStorage.getItem('selectedUserCompany');
     this.customerId = this.route.snapshot.params.id;
-    // standalone on route
-    // this.productId = this.route.snapshot.params.id
     if (action === 'new') {
       this.title = $localize`:@@customerDetail.newCustomer.title:New customer`;
       this.update = false;
-      // let resp = await this.chainOrganizationService.getOrganization(this.organizationId).pipe(take(1)).toPromise()
-      // if (resp && resp.status === 'OK') {
-      //   this.organization = resp.data
-      // }
     } else if (action === 'update') {
       this.title = $localize`:@@customerDetail.updateCustomer.title:Edit customer`;
       this.update = true;
@@ -91,23 +72,9 @@ export class CompanyCustomersDetailsComponent implements OnInit {
       if (res && res.status === 'OK') {
         this.customer = res.data;
       }
-      // let resp = await this.chainCompanyCustomerService.getCompanyCustomer(this.route.snapshot.params.companyCustomerId).pipe(take(1)).toPromise()
-      // if (resp && resp.status === 'OK') {
-      //   this.customer = resp.data;
-      //   this.organizationId = resp.data.organizationId;
-      // }
-      // let resp1 = await this.chainOrganizationService.getOrganization(this.organizationId).pipe(take(1)).toPromise()
-      // if (resp1 && resp1.status === 'OK') {
-      //   this.organization = resp1.data
-      // }
     } else {
       throw Error('Wrong action.');
     }
-    // let res = await this.chainProductService.getProductByAFId(this.productId).pipe(take(1)).toPromise();
-    // if (res && res.status === "OK" && res.data && dbKey(res.data)) {
-    //   this.chainProductId = dbKey(res.data);
-    //   this.chainProduct = res.data;
-    // }
   }
 
   newCustomer() {
