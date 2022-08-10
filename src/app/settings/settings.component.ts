4ea266f5324f4fae471500b0543ee37e3fe6b274
Pece Adjievski <pece.adzievski@sunesis.si>
2022-08-10 22:39:02 +0200

    WIP: removed api-chain dependencies

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -6,13 +6,6 @@ import { take } from 'rxjs/operators';
 import { NgbModalImproved } from '../core/ngb-modal-improved/ngb-modal-improved.service';
 import { Router } from '@angular/router';
 import { TypeDetailModalComponent } from './type-detail-modal/type-detail-modal.component';
-import { ProductControllerService } from 'src/api/api/productController.service';
-import { CompanyControllerService } from 'src/api/api/companyController.service';
-import { UserControllerService } from 'src/api/api/userController.service';
-import { OrganizationService } from 'src/api-chain/api/organization.service';
-import { UserService } from 'src/api-chain/api/user.service';
-import { ChainFileInfo } from 'src/api-chain/model/chainFileInfo';
-import { ProductService } from 'src/api-chain/api/product.service';
 import { TabCommunicationService } from '../shared/tab-communication.service';
 import { Subscription } from 'rxjs';
 import { ComponentCanDeactivate } from '../shared-services/component-can-deactivate';
@@ -80,13 +73,7 @@ export class SettingsComponent extends ComponentCanDeactivate implements OnInit,
     protected globalEventsManager: GlobalEventManagerService,
     protected commonController: CommonControllerService,
     protected modalService: NgbModalImproved,
-    protected router: Router,
-    protected productController: ProductControllerService,
-    protected companyController: CompanyControllerService,
-    protected userController: UserControllerService,
-    protected chainOrganizationService: OrganizationService,
-    protected chainUserService: UserService,
-    protected chainProductService: ProductService
+    protected router: Router
   ) {
     super();
   }
@@ -204,149 +191,6 @@ export class SettingsComponent extends ComponentCanDeactivate implements OnInit,
         if (type === 'semi-products') { this.parentReloadSemiProducts = !this.parentReloadSemiProducts; }
       }
     });
-  }
-
-  showTypes(type) {
-    this.router.navigate(['/', 'settings', type]).then();
-  }
-
-  async mapping() {
-    await this.userMapping();
-    await this.companyMapping();
-    await this.productMapping();
-  }
-
-  async companyMapping() {
-    try {
-      this.globalEventsManager.showLoading(true);
-      const resp = await this.companyController.listCompaniesAdminUsingGET().pipe(take(1)).toPromise();
-      if (resp && resp.status === 'OK' && resp.data && resp.data.items) {
-        for (const comp of resp.data.items) {
-          const respComp = await this.companyController.getCompanyUsingGET(comp.id).pipe(take(1)).toPromise();
-          if (respComp && 'OK' === respComp.status && respComp.data) {
-            const c = respComp.data;
-            const obj = {
-              id: c.id,
-              name: c.name,
-              email: c.email,
-              about: c.about,
-              phone: c.phone,
-              headquarters: c.headquarters,
-              logo: c.logo as ChainFileInfo,
-              manager: c.manager ? c.manager : '',
-              mediaLinks: c.mediaLinks ? c.mediaLinks : {},
-              webPage: c.webPage ? c.webPage : '',
-              abbreviation: c.abbreviation ? c.abbreviation : '',
-              entityType: 'company'
-            };
-            delete (obj.logo as any).id;
-            const res = await this.chainOrganizationService.postOrganization(obj).pipe(take(1)).toPromise();
-            if (res && 'OK' !== res.status) {
-              console.log('ORGANIZATION NOT imported to CHAIN', res);
-            }
-          } else {
-            console.log('CANNOT access COMPANY from JAVA apis', respComp);
-          }
-        }
-      }
-    } catch (e) {
-    } finally {
-      this.globalEventsManager.showLoading(false);
-    }
-  }
-
-  async userMapping() {
-    try {
-      this.globalEventsManager.showLoading(true);
-      const resp = await this.userController.adminListUsersUsingGET().pipe(take(1)).toPromise();
-      if (resp && resp.status === 'OK' && resp.data && resp.data.items) {
-
-        for (const user of resp.data.items) {
-          const obj = {
-            id: user.id,
-            email: user.email,
-            name: user.name,
-            surname: user.surname,
-            role: user.role,
-            status: user.status
-          };
-          const res = await this.chainUserService.postUser(obj as any).pipe(take(1)).toPromise();
-          if (res && 'OK' !== res.status) {
-            console.log('USER NOT imported to CHAIN', res);
-          }
-        }
-      }
-    } catch (e) {
-
-    } finally {
-      this.globalEventsManager.showLoading(false);
-    }
-  }
-
-  async productMapping() {
-    try {
-      this.globalEventsManager.showLoading(true);
-      const resp = await this.productController.listProductsAdminUsingGET().pipe(take(1)).toPromise();
-      if (resp && resp.status === 'OK' && resp.data && resp.data.items) {
-        for (const prod of resp.data.items) {
-          const respProd = await this.productController.getProductUsingGET(prod.id).pipe(take(1)).toPromise();
-          if (respProd && 'OK' === respProd.status && respProd.data) {
-            const p = respProd.data;
-            const organizationRoles = [];
-            if (p.associatedCompanies && p.associatedCompanies.length > 0) {
-              for (const com of p.associatedCompanies) {
-                const assocComp = {
-                  companyId: com.company.id,
-                  role: com.type
-                };
-                organizationRoles.push(assocComp);
-              }
-            }
-            const labels = [];
-            const respProd1 = await this.productController.getProductLabelsUsingGET(prod.id).pipe(take(1)).toPromise();
-            if (respProd1 && 'OK' === respProd1.status && respProd1.data) {
-              for (const d of respProd1.data) {
-                const respProd2 = await this.productController.getProductLabelValuesUsingGET(d.id).pipe(take(1)).toPromise();
-                if (respProd2 && 'OK' === respProd2.status && respProd2.data) {
-                  labels.push(respProd2.data);
-                } else {
-                  console.log('CANNOT ACCESS ProductLabelValues or no data', respProd2);
-                }
-              }
-            } else {
-              console.log('CANNOT ACCESS ProductLabelsValues or no data', respProd1);
-            }
-            const obj = {
-              id: p.id,
-              name: p.name,
-              description: p.description,
-              howToUse: p.howToUse ? p.howToUse : '',
-              ingredients: p.ingredients ? p.ingredients : '',
-              nutritionalValue: p.nutritionalValue ? p.nutritionalValue : '',
-              origin: p.origin,
-              photo: p.photo,
-              process: p.process,
-              responsibility: p.responsibility,
-              sustainability: p.sustainability,
-              keyMarketsShare: p.keyMarketsShare ? p.keyMarketsShare : {},
-              companyId: p.company && p.company.id ? p.company.id : null,
-              organizationRoles,
-              labels
-            };
-            const res = await this.chainProductService.postProduct(obj).pipe(take(1)).toPromise();
-            if (res && 'OK' !== res.status) {
-              console.log('PRODUCT NOT imported to CHAIN', res);
-            }
-          } else {
-            console.log('CANNOT access PRODUCT from JAVA apis', respProd);
-          }
-        }
-      }
-    } catch (e) {
-
-    } finally {
-      this.globalEventsManager.showLoading(false);
-    }
   }
 
 }
