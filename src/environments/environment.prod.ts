016131507c3b265cea84dadeda5bbf65cf16cdb1
Pece Adjievski <pece.adzievski@sunesis.si>
2023-03-14 20:06:01 +0100

    Release v2.29.1

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -14,7 +14,7 @@ export const environment = {
     relativeFileUploadUrlManualType: window['env']['relativeFileUploadUrlManualType'] || '',
     relativeImageUploadUrl: window['env']['relativeImageUploadUrl'] || '',
     relativeImageUploadUrlAllSizes: window['env']['relativeImageUploadUrlAllSizes'] || '',
-    version: '2.29.1-SNAPSHOT',
+    version: '2.29.1',
 
     googleMapsApiKey: window['env']['googleMapsApiKey'] || '',
     googleAnalyticsId: '',
