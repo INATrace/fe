bf467eec425e5e645b1f0c63b2b0e8e81fed2b9b
Pece Adjievski <pece.adzievski@sunesis.si>
2023-05-11 12:04:01 +0200

    Release v2.32.1

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -14,7 +14,7 @@ export const environment = {
     relativeFileUploadUrlManualType: window['env']['relativeFileUploadUrlManualType'] || '',
     relativeImageUploadUrl: window['env']['relativeImageUploadUrl'] || '',
     relativeImageUploadUrlAllSizes: window['env']['relativeImageUploadUrlAllSizes'] || '',
-    version: '2.32.0',
+    version: '2.32.1',
 
     googleMapsApiKey: window['env']['googleMapsApiKey'] || '',
     googleAnalyticsId: '',
