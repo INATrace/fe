4a7a6b1ac864bd433c9e04bc8520e0a1d0e75080
Pece Adjievski <pece.adzievski@sunesis.si>
2025-02-19 14:15:16 +0100

    Updated to next SNAPSHOT version

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -14,7 +14,7 @@ export const environment = {
     relativeFileUploadUrlManualType: window['env']['relativeFileUploadUrlManualType'] || '',
     relativeImageUploadUrl: window['env']['relativeImageUploadUrl'] || '',
     relativeImageUploadUrlAllSizes: window['env']['relativeImageUploadUrlAllSizes'] || '',
-    version: '2.39.0-SNAPSHOT',
+    version: '2.40.0-SNAPSHOT',
 
     googleMapsApiKey: window['env']['googleMapsApiKey'] || '',
     googleAnalyticsId: '',
